6774183664c926370c3f72de5320942adf3dc3f0
DanBetTap <134719509+DanBetTap@users.noreply.github.com>
2024-05-17 23:59:38 -0400

    cambios css y demas

--- Trabajo-Web-main/Trabajo Web/js/Scripts.js
+++ Trabajo-Web-main/Trabajo Web/js/Scripts.js
@@ -1,4 +1,4 @@
-function validar(rut,nombre,correo,edad,contraseña,direccion){
+/*function validar(rut,nombre,correo,edad,contraseña,direccion){
     var email = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
     var name = /^[a-zA-Z]+$/;
     if(String(rut).length<8 || String(rut).length>9){
@@ -37,3 +37,24 @@ function validar() {
         document.getElementById("resultado").innerHTML = "<div class='alert alert-danger w-50 mx-auto text-center'>Usuario debe tener minimo 5 y maximo 20 caracteres</div>"
     }
 }
+
+$(document).ready(function(){
+            
+    let user = document.getElementById("nombre").value;
+    let pass = document.getElementById("contraseña").value;
+    if (String(user).length >= 5) {
+        if (String(pass).length > 3) {
+            document.getElementById("nombre").style.border = "1px solid lightgrey";
+            document.getElementById("contraseña").style.border = "1px solid lightgrey";
+            document.getElementById("resultado").innerHTML = "<div class='alert alert-success w-50 mx-auto text-center'>" +
+                "Acceso Concedido</div>"
+        } else {
+            document.getElementById("contraseña").style.border = "1px solid red";
+            document.getElementById("resultado").innerHTML = "<div class='alert alert-danger w-50 mx-auto text-center'>" +
+                "Contraseña debe tener minimo 8 caracteres</div>"
+        }
+    } else {
+        document.getElementById("nombre").style.border = "1px solid red";
+        document.getElementById("resultado").innerHTML = "<div class='alert alert-danger w-50 mx-auto text-center'>Usuario debe tener minimo 5 y maximo 20 caracteres</div>"
+    }
+})*/
